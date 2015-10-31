dd67228cd878b975dc50fbe14d8e5aca115e18f3
treykeown <jfktrey@gmail.com>
2015-10-31 16:10:57 -0500

    Fixes for IE, removal of touch highlight, easier to play/pause on mobile.

--- main.js
+++ main.js
@@ -95,33 +95,29 @@ function checkForOutsideAction () {
 
 // Function to handle the displaying of the loading screens.
 function viewHandler (event) {
-	switch (event) {
+	// Once both the player and window have been fully loaded
+	if (event === "loaded") {
+		// If the audio is locked and touch required, we pull a trick to unlock the audio context.
+		if (audioLocked) {
+			nodes.player.className = "takeover";	// Make the player button cover the screen (and make it fully transparent)
+			nodes.touch.style.display = "inline";	// Show the "touch to begin" text
 
-		// Once both the player and window have been fully loaded
-		case "loaded":
-			// If the audio is locked and touch required, we pull a trick to unlock the audio context.
-			if (audioLocked) {
-				nodes.player.className = "takeover";	// Make the player button cover the screen (and make it fully transparent)
-				nodes.touch.style.display = "inline";	// Show the "touch to begin" text
-				
-				var toHide = $class("wait-please");
-				for (var i = 0, current; current = toHide.item(i); i++) {
-					current.style.display = "none";		// Hide the loading texts
-				}
-			// Otherwise, just show Casey's head (for non-mobile devices)
-			} else {
-				nodes.loading.style.display = "none";
+			var toHide = $class("wait-please");
+			for (var i = 0, current; current = toHide.item(i); i++) {
+				current.style.display = "none";		// Hide the loading texts
 			}
-			break;
-
-		// For mobile devices ONLY - when the touch event that unlocks the audio context has been triggered.
-		case "touched":
-			nodes.player.className = "passive";
+		// Otherwise, just show Casey's head (for non-mobile devices)
+		} else {
 			nodes.loading.style.display = "none";
-			break;
+		}
 
-		default:
-			console.error("Invalid event passed to viewHandler: " + event);
+	// For mobile devices ONLY - when the touch event that unlocks the audio context has been triggered.
+	} else if (event === "touched") {
+		nodes.player.className = "passive";
+		nodes.loading.style.display = "none";
+
+	} else {
+		console.error("Invalid event passed to viewHandler: " + event);
 	}
 }
 
@@ -172,6 +168,12 @@ function setCasey (state) {
 }
 
 
+// Determine whether Casey's mouth is currently open or closed.
+function getCasey () {
+	return (nodes.casey.src.slice(-8) === "open.png") ? "open" : "closed";
+}
+
+
 // Start up the soundcloud widget iframe and attach what events we need.
 function createPlayer (playlistNumber) {
 	var frame = document.createElement("iframe");
@@ -217,6 +219,56 @@ function createPlayer (playlistNumber) {
 }
 
 
+// Handler for when an action is taken that should toggle play/pause.
+// On mobile, that's tapping nodes.mouth
+// On non-mobile, that's clicking svgNodes.hitbox
+// They're different because it's pretty hard to tap the hitbox on a normal phone screen.
+function controlsEventHandler (e) {
+	e.stopPropagation();	// Stop propagation up to the <body> event handler, which will close Casey's mouth.
+
+	// If we're not already in the process of pausing (waiting for the head to bobble back to the center)
+	if (player.state !== player.STATES.PAUSING) {
+
+		// If we're on mobile and his mouth isn't already open, just open his mouth.
+		if (audioLocked && (getCasey() === "closed")) {
+			setCasey("open");
+
+		// If we're not on mobile...
+		} else {
+
+			// If we're already playing, then pause the player.
+			if (player.state === player.STATES.PLAYING) {
+				player.state = player.STATES.PAUSING;
+
+				player.handle.pause();
+
+				setControls(player.STATES.PAUSING);
+				console.info("%cStopped playing track.", "color: indianred");
+
+				if (audioLocked) setCasey("open");
+
+			// Otherwise, start playing.
+			} else {
+				player.state = player.STATES.PLAYING;
+
+				var newTrack = Math.floor(Math.random() * player.songCount);
+				player.handle.skip(newTrack);
+
+				setControls(player.STATES.PLAYING);
+				console.info("Now playing track %c#" + newTrack, "color: limegreen");
+
+				// Automatically close Casey's mouth if we're working with a touchscreen.
+				if (audioLocked) {
+					setTimeout(function () {
+						setCasey("closed");
+					}, 2000);
+				}
+			}
+		}
+	}
+}
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -229,9 +281,6 @@ window.addEventListener("load", function (e) {
 
 // Once the DOM has been fully loaded, we can start doing things.
 document.addEventListener("DOMContentLoaded", function () {
-	// :)
-	console.info("%cGreetings, curious internet citizen! This is an open source project.\n\nYou can find out more about this website on GitHub at https://github.com/jfktrey/vlog-radio", "color: blue; font-size: large");
-
 	// Set up the nodes object
 	var nodeKeys = Object.keys(nodes);
 	for (var i = 0, node; node = nodeKeys[i]; i++) {	// My favorite way of looping over non-sparse arrays
@@ -239,33 +288,38 @@ document.addEventListener("DOMContentLoaded", function () {
 	}
 	nodes.player = createPlayer(player.playlist);
 
-	// Set up node event handlers
+	// If we're on mobile, set up extra "click" (actually tap) events for body and mouth
 	if (audioLocked) {
-		document.body.style.cursor = "pointer";			// Necessary to make the body clickable for iOS.
-		document.body.addEventListener("click", function () {
+		// Disable scrolling and highlighting
+		document.addEventListener("touchstart", function (e) {}, true);
+		document.addEventListener("touchmove",  function (e) {
+			e.preventDefault();
+		}, true);
+
+		document.body.style.cursor = "pointer";				// Necessary to make the body clickable for iOS.
+		document.body.addEventListener("click", function (e) {
+			e.stopPropagation();
 			setCasey("closed");
 		});
 
-		nodes.mouth.style.cursor = "pointer";					// Necessary to make the mouth clickable for iOS.
-		nodes.mouth.addEventListener("click", function (e) {
-			e.stopPropagation();
-			setCasey("open");
-		});
+		nodes.mouth.style.cursor = "pointer";							// Necessary to make the mouth clickable for iOS.
+		nodes.mouth.addEventListener("click", controlsEventHandler);	// Use the whole mouth as the play/pause button on mobile since it's harder to tap precisely.
 
+		// Switch to the mobile explanation image, which has "tap" instead of "hover"
 		nodes.explain.src = "./images/explanation-mobile.svg";
+	// Otherwise, set up hover events for mouse-based devices
+	} else {
+		nodes.mouth.addEventListener("mouseover", function () { setCasey("open")   });
+		nodes.mouth.addEventListener("mouseout",  function () { setCasey("closed") });
 	}
 
-	// Hover events for the mouth
-	nodes.mouth.addEventListener("mouseover", function () { setCasey("open")   });
-	nodes.mouth.addEventListener("mouseout",  function () { setCasey("closed") });
-
 	// Set the ratio property of the image and resize based on that.
-	nodes.casey.addEventListener("load", (function () {
-		return function () {
-			nodes.casey.dataset.ratio = nodes.casey.naturalWidth / nodes.casey.naturalHeight;	// Width/Height of Casey's image
-			triggerEvent("resize", window);														// Initial sizing call
-		};
-	})());
+	nodes.casey.addEventListener("load", function () {
+		nodes.casey.dataset.ratio = nodes.casey.naturalWidth / nodes.casey.naturalHeight;	// Width/Height of Casey's image
+		triggerEvent("resize", window);														// Initial sizing call
+	});
+
+	nodes.casey.src = nodes.casey.src;	// Hack for IE, since it has DOMContentLoaded trigger before a cached image's load event...
 
 	// Once the controls load, attach events to the SVG elements within
 	nodes.controls.addEventListener("load", function () {
@@ -278,55 +332,24 @@ document.addEventListener("DOMContentLoaded", function () {
 			svgNodes[node] = $controlsId(svgNodes[node]);
 		}
 
-		// When the hitbox is clicked, play or pause depending on the current state.
-		svgNodes.hitbox.addEventListener("click", function (e) {
-			e.stopPropagation();	// Don't let this bubble up to <body>. But would it? Would it bubble through the SVG document? Not sure. TODO: test this.
-
-			// If we're not already in the process of pausing (waiting for the head to bobble back to the center)
-			if (player.state !== player.STATES.PAUSING) {
-
-				// If we're already playing, then pause the player.
-				if (player.state === player.STATES.PLAYING) {
-					player.state = player.STATES.PAUSING;
-
-					player.handle.pause();
-
-					setControls(player.STATES.PAUSING);
-					console.info("%cStopped playing track.", "color: indianred");
-
-					if (audioLocked) setCasey("open");
-
-				// Otherwise, start playing.
-				} else {
-					player.state = player.STATES.PLAYING;
-
-					var newTrack = Math.floor(Math.random() * player.songCount);
-					player.handle.skip(newTrack);
-
-					setControls(player.STATES.PLAYING);
-					console.info("Now playing track %c#" + newTrack, "color: limegreen");
-
-					// Automatically close Casey's mouth if we're working with a touchscreen.
-					if (audioLocked) {
-						setTimeout(function () {
-							setCasey("closed");
-						}, 2000);
-					}	
-				}
-			}
-		});
+		// For non-mobile, when the hitbox is clicked, play or pause depending on the current state.
+		if (!audioLocked) {
+			svgNodes.hitbox.addEventListener("click", controlsEventHandler);
+		}
 	});
 
 	// Sorry, some JS-driven sizing is necessary. I really tried to avoid this.
 	window.addEventListener("resize", function () {
 		nodes.center.style.width   = nodes.casey.dataset.ratio * nodes.center.clientHeight + "px";
 		nodes.explain.style.bottom = (0.5 * nodes.center.clientHeight) - (0.5 * nodes.casey.clientHeight) + "px";
-		nodes.mouth.style.bottom   = 0.208 * nodes.casey.clientHeight + "px";
+		nodes.mouth.style.bottom   = 0.205 * nodes.casey.clientHeight + "px";
 	});
 
 	// Make an initial call to the sizer
 	triggerEvent("resize", window);
 });
 
+// =)
+console.info("%cGreetings, curious internet citizen! This is an open source project.\n\nYou can find out more about this website on GitHub at https://github.com/jfktrey/vlog-radio", "color: blue; font-size: large");
 
 })();
